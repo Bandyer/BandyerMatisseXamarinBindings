dacd59eaba23a242d20d399a2e7792fac1adb0a8
Kristiyan Petrov <petrov.kristiyan@gmail.com>
2020-05-19 17:54:39 +0200

    Initial commit

---
+++ DemoMatisse/MainActivity.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using Com.Zhihu.Matisse;
+using Com.Zhihu.Matisse.Engine.Impl;
+using Google.Android.Material.FloatingActionButton;
+
+namespace DemoMatisse
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            AndroidX.AppCompat.Widget.Toolbar toolbar = FindViewById<AndroidX.AppCompat.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View)sender;
+
+
+            Matisse.From(this)
+        .Choose(MimeType.OfAll())
+        .Countable(true)
+        .MaxSelectable(9)
+        .ThumbnailScale(0.85f)
+        .ImageEngine(new PicassoEngine())
+        .ShowPreview(false) // Default is `true`
+        .ForResult(123);
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            if (requestCode == 123 && resultCode == Result.Ok)
+            {
+                var mSelected = Matisse.ObtainResult(data);
+                Log.Error("Matisse", "mSelected: " + mSelected);
+            }
+
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# Bandyer's Matisse for Xamarin [![NuGet Badge](https://buildstats.info/nuget/Bandyer.Matisse)](https://www.nuget.org/packages/Bandyer.Matisse/)
+
+Port of https://github.com/bandyer/Matisse to Xamarin.
+
+## How to use
+
+Have a look at [README.md](https://github.com/bandyer/Matisse)
